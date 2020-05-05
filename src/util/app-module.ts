19af9be6075bd9d818acc1e57d541cf898108cfe
wezley <wezley.singleton@gmail.com>
2020-05-05 17:23:20 -0400

    pushing spa demo

---
+++ src/util/app-module.ts
@@ -0,0 +1,32 @@
+import { Router } from "./router.js";
+import { LoginComponent } from "../components/login-component.js";
+import { Route } from "./route.js";
+import { AuthService } from "../services/auth-service.js";
+import { RegisterComponent } from "../components/register-component.js";
+import { DashboardComponent } from "../components/dashboard-component.js";
+
+// Poor-man's Dependency Injection
+export class AppModule {
+
+    components = {};
+    services = {};
+
+    constructor() {
+
+        this.services['router'] = new Router();
+        this.services['auth'] = new AuthService();
+
+        this.components['login'] = new LoginComponent(this.services['auth'], this.services['router']);
+        this.components['register'] = new RegisterComponent();
+        this.components['dashboard'] = new DashboardComponent();
+
+        let routes = [
+            new Route('/login', this.components['login']),
+            new Route('/register', this.components['register']),
+            new Route('/dashboard', this.components['dashboard'])
+        ];
+
+        this.services['router'].addRoutes(routes);
+
+    }
+}

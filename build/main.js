085f20209e6e1620001905025977ee2fcbca0b17
jbostik <ebiven1563@gmail.com>
2020-05-08 12:30:45 -0400

    ...

--- build/main.js
+++ build/main.js
@@ -1,5 +1,5 @@
 import { AppModule } from "./util/app-module.js";
-export const API_URL = 'http://ec2-18-224-72-60.us-east-2.compute.amazonaws.com:8080';
+export const API_URL = 'https://ec2-18-224-72-60.us-east-2.compute.amazonaws.com:8080';
 window.onload = () => {
     console.log('page loaded.');
     const app = new AppModule();

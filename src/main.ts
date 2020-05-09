6c85d3f2d1b43ceae5fcb83f199089a821961e1b
wezley <wezley.singleton@gmail.com>
2020-05-09 13:39:05 -0400

    update api url to eb app endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { AppModule } from "./util/app-module.js";
 
-export const API_URL = 'http://ec2-34-192-223-121.compute-1.amazonaws.com:8080';
+export const API_URL = 'http://revaboardsapi-env-1.eba-53denqsk.us-east-1.elasticbeanstalk.com'
 
 window.onload = () => {
     console.log('page loaded.');

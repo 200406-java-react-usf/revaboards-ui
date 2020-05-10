1587b90f24c6523fc9048dfa7dee6bcecd487e40
wezley <wezley.singleton@gmail.com>
2020-05-10 18:52:16 -0400

    added small change to verify proper deployment

--- src/components/dashboard-component.ts
+++ src/components/dashboard-component.ts
@@ -4,7 +4,7 @@ import { state } from "../util/state.js";
 export class DashboardComponent implements View {
 
     template = `
-        <h1>Welcome, <span id="welcome-username"></span></h1>
+        <h1>Welcome, <span id="welcome-username"></span>!</h1>
     `;
 
     constructor() {
